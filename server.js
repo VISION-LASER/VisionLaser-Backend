68f6b93bbc9fcbbca45a1432fe8092b568bc195c
onja07 <onjaniainamapionona@gmail.com>
2026-05-28 16:19:43 +0300

    modif back

--- server.js
+++ server.js
@@ -38,11 +38,13 @@
 require('dotenv').config();
 const corsMiddleware = require('./src/middleware/cors.middleware');
 const connectDatabase = require('./src/config/connectDatabase');
+const path = require('path'); // pour upload image
 
 const express = require('express');
 
 // ✅ AJOUTEZ CETTE LIGNE
 const userRoutes = require('./src/routes/user.routes');
+const uploadRoutes = require('./src/routes/upload.routes');  // pour upload image
 
 const app = express();
 
@@ -52,6 +54,8 @@ app.use(corsMiddleware);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // pour upload image
 
 // Connexion BDD
 connectDatabase()
@@ -64,6 +68,7 @@ connectDatabase()
 
 // ✅ AJOUTEZ CES LIGNES (routes d'authentification)
 app.use('/api/users', userRoutes);
+app.use('/api/users', uploadRoutes); // pour upload image
 
 // Route test
 app.get('/', (req, res) => {
@@ -81,4 +86,6 @@ app.listen(PORT, () => {
     console.log(`   POST   http://localhost:${PORT}/api/users/login`);
     console.log(`   POST   http://localhost:${PORT}/api/users/refresh`);
     console.log(`   GET    http://localhost:${PORT}/api/users/profile (protégée)`);
+    console.log(`Routes d'upload disponibles:`);
+    console.log(`   POST   http://localhost:${PORT}/api/users/upload`);
 });
